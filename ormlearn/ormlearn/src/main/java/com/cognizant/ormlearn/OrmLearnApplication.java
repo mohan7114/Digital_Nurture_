b0f75d7023820a5202b44bb59f9cd3bf16d492f6
mohana sundaram <mohanasundaram7114.com>
2025-07-06 18:43:00 +0530

    Initial Commit

---
+++ ormlearn/ormlearn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,56 @@
+package com.cognizant.ormlearn;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.model.Employee;
+import com.cognizant.ormlearn.service.CountryService;
+import com.cognizant.ormlearn.service.EmployeeService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    private static CountryService countryService;
+    private static EmployeeService employeeService;
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        LOGGER.info("Inside main");
+
+        // Load services from Spring context
+        countryService = context.getBean(CountryService.class);
+        employeeService = context.getBean(EmployeeService.class);
+
+        testGetAllCountries();
+        testAddEmployee();
+    }
+
+    private static void testGetAllCountries() {
+        LOGGER.info("Start testGetAllCountries");
+        List<Country> countries = countryService.getAllCountries();
+        LOGGER.debug("countries={}", countries);
+        LOGGER.info("End testGetAllCountries");
+    }
+
+    private static void testAddEmployee() {
+        LOGGER.info("Start testAddEmployee");
+
+        Employee emp = new Employee();
+        emp.setName("Alice");
+        emp.setSalary(55000.0);
+
+        employeeService.addEmployee(emp);
+
+        LOGGER.info("Employee added successfully");
+        LOGGER.info("End testAddEmployee");
+    }
+}
